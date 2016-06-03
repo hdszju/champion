813439d02fec158c37ed39ff82813b056222382f
hds <hdszju@163.com>
2016-06-03 22:24:02 +0800

    modify demo.sh

--- demo.sh
+++ demo.sh
@@ -5,4 +5,6 @@ gnome-terminal -x bash -c " roslaunch cp_tasks fake_turtlebot.launch"
 sleep 5s
 gnome-terminal -x bash -c " rosrun rviz rviz -d `rospack find cp_tasks`/nav_tasks.rviz"
 sleep 5s
+gnome-terminal -x bash -c " rosrun cp_tasks patrol_script.py"
+sleep 5s
 gnome-terminal -x bash -c " roslaunch cp_vel cp_vel.launch"
