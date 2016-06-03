f30b3a4233baf8d426e1828bc7e4a7666b65fe7f
Dashun Han <hdszju@163.com>
2016-06-04 01:08:55 +0800

    cao

--- prereq.sh
+++ prereq.sh
@@ -1,5 +1,22 @@
 #!/binsh
 
+#Install ROS
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net --recv-key 0xB01FA116
+
+sudo apt-get update
+
+sudo apt-get install ros-indigo-desktop-full
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt-get install python-rosinstall
+
 # Install the prerequisites for the champion patrol projects
 
 sudo apt-get install ros-indigo-arbotix ros-indigo-openni-camera \
