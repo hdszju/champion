e49c28b033724ae3cc3410914cef19d9c48a3041
Dashun Han <hdszju@163.com>
2016-06-11 01:50:59 +0800

    modify error

--- demo/map_create/run.sh
+++ demo/map_create/run.sh
@@ -2,19 +2,17 @@
 gnome-terminal -x bash -c " roslaunch pr2_gazebo pr2_wg_world.launch"
 sleep 15s
 
-gnome-terminal -x bash -c " rosbag record -O mylaserdata /base_scan /tf"
-sleep 5s
-
-
 gnome-terminal -x bash -c " rosparam set use_sim_time true;\
                             rosparam set delta 0.1;\
                             rosparam set xmin -15.0;\
                             rosparam set xmax 15.0;\
                             rosparam set ymin -15.0;\
-                            rosparam set yman 15.0;\
+                            rosparam set ymax 15.0;\
                             rosrun gmapping slam_gmapping scan:=base_scan _odom_frame:=odom_combined"
 sleep 5s
 
 
 gnome-terminal -x bash -c " roslaunch pr2_teleop teleop_keyboard.launch"
 sleep 5s
+
+rosbag record -O mylaserdata /base_scan /tf
