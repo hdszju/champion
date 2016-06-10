0087763db2f15dd46b5707e64754a37d8f09723a
Dashun Han <hdszju@163.com>
2016-06-11 02:42:44 +0800

    adsf

--- demo/map_create/run.sh
+++ demo/map_create/run.sh
@@ -4,10 +4,10 @@ sleep 15s
 
 gnome-terminal -x bash -c " rosparam set use_sim_time true;\
                             rosrun gmapping slam_gmapping \
-                            xmin=-15.0  \
-                            xmax=15.0 \
-                            ymin=-15.0 \
-                            ymax=15.0 \
+                            _xmin:=-15.0  \
+                            _xmax:=15.0 \
+                            _ymin:=-15.0 \
+                            _ymax:=15.0 \
                             scan:=base_scan _odom_frame:=odom_combined"
 sleep 5s
 
