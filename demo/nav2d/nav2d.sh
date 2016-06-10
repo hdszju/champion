884dfdd2d3f5748ae38627a958e1621f02d996cc
Dashun Han <hdszju@163.com>
2016-06-10 22:09:28 +0800

    modify gnome-terminal

--- demo/nav2d/nav2d.sh
+++ demo/nav2d/nav2d.sh
@@ -9,12 +9,27 @@ sudo rm -rf /var/www/html/nav2d.html
 sudo cp -a ./js_nav2d /var/www/html/
 sudo cp -a ./nav2d.html /var/www/html/
 
-roslaunch pr2_gazebo pr2_wg_world.launch
-export ROBOT=sim
-#rosrun map_server map_server /opt/ros/groovy/stacks/wg_common/willow_maps/willow-sans-whitelab-2010-02-18-0.025.pgm 0.025
-roslaunch pr2_2dnav pr2_2dnav.launch
 
-roslaunch pr2_tuckarm tuck_arms.launch
-rosrun robot_pose_publisher robot_pose_publisher
-roslaunch rosbridge_server rosbridge_websocket.launch
+
+
+gnome-terminal -x bash -c " export ROBOT=sim;roslaunch pr2_2dnav pr2_2dnav.launch"
+sleep 5s
+
+gnome-terminal -x bash -c " roslaunch pr2_tuckarm tuck_arms.launch"
+sleep 5s
+
+
+gnome-terminal -x bash -c " rosrun robot_pose_publisher robot_pose_publisher"
+sleep 5s
+
+
+gnome-terminal -x bash -c " roslaunch rosbridge_server rosbridge_websocket.launch"
+sleep 5s
+
+#rosrun map_server map_server /opt/ros/groovy/stacks/wg_common/willow_maps/willow-sans-whitelab-2010-02-18-0.025.pgm 0.025
+
+
+
+
+
 sudo service apache2 restart
