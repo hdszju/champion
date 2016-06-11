74f31bbb7994a5d2aedc2c3039eca31d0f653123
Dashun Han <hdszju@163.com>
2016-06-11 08:53:55 +0800

    jiushi

--- demo/nav2d/nav2d.sh
+++ demo/nav2d/nav2d.sh
@@ -12,7 +12,7 @@ sudo cp -a ./nav2d.html /var/www/html/
 
 
 
-gnome-terminal -x bash -c " export ROBOT=sim; roslaunch cp_vel pr2_2dnav.launch"
+gnome-terminal -x bash -c " export ROBOT=sim;rosparam set use_sim_time true; roslaunch cp_vel pr2_2dnav.launch"
 sleep 5s
 
 gnome-terminal -x bash -c " roslaunch pr2_tuckarm tuck_arms.launch"
